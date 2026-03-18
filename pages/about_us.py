e759e1519df06c19717d116e132a46921af5cfbc
Truong Kool <truongthanhh101@gmail.com>
2026-03-18 16:57:59 +0700

    Update about_us.py

--- pages/about_us.py
+++ pages/about_us.py
@@ -1,6 +1,22 @@
 import streamlit as st
 
-st.set_page_config(page_title="About Us", page_icon="ℹ️")
+@st.dialog("About Us")
+def show_about_us():
+    st.write("""
+    This was created to shit on Viery, as he dares to request 800 chapters, so i'm creating this to avoid doing his requests.
+Any bug problems u can send a dm though @thanhdeptrai101 (i'm not gonna reply to it mueheheheheh) 
+
+Thanks to our contributors  
+Viery: the sole reason i create this bullshit
+Kuroh: for being blac
+Hb: for being balls
+Bobo: for being dumb
+Nekoldun: for translating ts
+Vlya: for being emotional support
+    """)
+
+if st.button("ℹ️ About Us"):
+    show_about_us()
 
 st.title("About Us")
 st.write("""
@@ -14,5 +30,4 @@ Hb: for being balls
 Bobo: for being dumb
 Nekoldun: for translating ts
 Vlya: for being emotional support
-
 """)
