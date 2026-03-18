c6e92eb6d8411a7b905dd3d7295152b7c082c86e
Truong Kool <truongthanhh101@gmail.com>
2026-03-18 17:09:18 +0700

    Update about_us.py

--- pages/about_us.py
+++ pages/about_us.py
@@ -6,14 +6,14 @@ def show_about_us():
     This was created to shit on Viery, as he dares to request 800 chapters, so i'm creating this to avoid doing his requests.
 Any bug problems u can send a dm though @thanhdeptrai101 (i'm not gonna reply to it mueheheheheh) 
 
-Thanks to our contributors  
-Viery: the sole reason i create this bullshit
-Kuroh: for being blac
-Hb: for being balls
-Bobo: for being dumb
-Nekoldun: for translating ts
-Vlya: for being emotional support
-Tyuyu    """)
+Thanks to our contributors:
+Viery: the sole reason i create this bullshit.
+Kuroh: for being blac.
+Hb: for being balls.
+Bobo: for being dumb.
+Nekoldun: for translating ts.
+Vlya: for being emotional support.
+Tyuyu.    """)
 
 if st.button("ℹ️ About Us"):
     show_about_us()
@@ -21,14 +21,14 @@ if st.button("ℹ️ About Us"):
 st.title("About Us")
 st.write("""
 This was created to shit on Viery, as he dares to request 800 chapters, so i'm creating this to avoid doing his requests.
-Any bug problems u can send a dm though @thanhdeptrai101 (i'm not gonna reply to it mueheheheheh) 
+Any bug problems u can send a dm though @thanhdeptrai101 (i'm not gonna reply to it mueheheheheh). 
 
-Thanks to our contributors  
-Viery: the sole reason i create this bullshit
-Kuroh: for being blac
-Hb: for being balls
-Bobo: for being dumb
-Nekoldun: for translating ts
-Vlya: for being emotional support
-Tyuyuyuyuyuy
+Thanks to our contributors:
+Viery: the sole reason i create this bullshit.
+Kuroh: for being blac.
+Hb: for being balls.
+Bobo: for being dumb.
+Nekoldun: for translating ts.
+Vlya: for being emotional support.
+Tyuyuyuyuyuy.
 """)
